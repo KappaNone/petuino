5cb53fb6ab99f004c49929df0ea14307426c7209
Milana Doborjginidze <milanafrombatumi@gmail.com>
2025-06-21 01:19:09 +0200

    Update Scrolling_menu.cpp
    Fixed coding conventions

--- main/Scrolling_menu.cpp
+++ main/Scrolling_menu.cpp
@@ -2,46 +2,63 @@
 #include "Scrolling_menu.h"
 
 
-Scrolling_menu::Scrolling_menu(int posX, int posY, int menuSize){
+Scrolling_menu::Scrolling_menu(int posX, int posY, int menuSize)
+{
   _menuSize = menuSize - 1;
   x = posX;
   y = posY;
 }
 
-void Scrolling_menu::moveRight(){
-  if (nextSection == _menuSize){
+void Scrolling_menu::moveRight()
+{
+  if (nextSection == _menuSize)
+  {
     nextSection = 0;
     activeSection++;
     prevSection++;
-  }else if (activeSection == _menuSize){
+  }
+  else if (activeSection == _menuSize)
+  {
     nextSection++;
     activeSection = 0;
     prevSection++;
-  }else if (prevSection == _menuSize){
+  }
+  else if (prevSection == _menuSize)
+  {
     nextSection++;
     activeSection++;
     prevSection = 0;
-  }else {
+  }
+  else
+  {
     nextSection++;
     activeSection++;
     prevSection++;
   }
 }
 
-void Scrolling_menu::moveLeft(){
-  if (prevSection == 0){
+void Scrolling_menu::moveLeft()
+{
+  if (prevSection == 0)
+  {
     nextSection--;
     activeSection--;
     prevSection = _menuSize;
-  }else if (activeSection == 0){
+  }
+  else if (activeSection == 0)
+  {
     nextSection--;
     activeSection = _menuSize;
     prevSection--;
-  }else if (nextSection == 0){
+  }
+  else if (nextSection == 0)
+  {
     nextSection = _menuSize;
     activeSection--;
     prevSection--;
-  }else {
+  }
+  else
+  {
     nextSection--;
     activeSection--;
     prevSection--;
